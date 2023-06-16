2073e37d0742303ddaf8acee02064a0cd73ad5a6
Michal Dziekonski (mdz) <michal.dziekonski+github@gmail.com>
2023-06-16 04:10:14 +0200

    GH-1 | Refactor JS code (#2)
    * GH-1 | Cleanup code
    
    * GH-1 | Unpack some config beforehand
    
    * GH-1 | Reindent file
    
    * GH-1 | Fix typo
    
    * GH-1 | Clarify plugin settings var
    
    * GH-1 | Simplify parameters extraction
    
    * GH-1 | For minification, change to SPDX for copyright
    
    * GH-1 | Use existing constant

--- octoprint_Change_Filament/static/js/Change_Filament.js
+++ octoprint_Change_Filament/static/js/Change_Filament.js
@@ -1,106 +1,123 @@
-/*
- * OctoPrint Change_Filament plugin.
- *
- * Copyright (c) 2019, Jim Pingle <jim@pingle.org>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/**
+ * Copyright 2019, Jim Pingle <jim@pingle.org> and the OctoPrint Change_Filament plugin contributors
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 $(function() {
-	function Change_filamentViewModel(parameters) {
-		var self = this;
+    const preParkPauseGCODEs = [
+        'M0',
+    ];
+    const preParkHomeGCODEs = [
+        'G28 X0 Y0',
+    ];
+    const preParkExtrudeGCODEs = [
+        'M83',
+        'G1 E-5 F50',
+    ];
 
-		self.settings = parameters[0];
-		self.controlViewModel = parameters[1];
+    function Change_filamentViewModel(parameters) {
+        const self = this;
 
-		self.getAdditionalControls = function() {
-			var settings = self.settings.settings.plugins.Change_Filament;
+        const [ settingsViewModel, controlViewModel ] = parameters;
 
-			var preparkpause = '';
-			if (settings.pause_before_park() != false) {
-				preparkpause = 'M0';
-			}
+        self.getAdditionalControls = function() {
+            const pluginSettings = settingsViewModel.settings.plugins.Change_Filament;
 
-			var preparkhome = '';
-			if (settings.home_before_park() != false) {
-				preparkhome = 'G28 X0 Y0';
-			}
+            const setPreParkPauseGCODEs = (
+                (pluginSettings.pause_before_park() != false)
+                ? preParkPauseGCODEs
+                : []
+            );
+            const setPreParkHomeGCODEs = (
+                (pluginSettings.home_before_park() != false)
+                ? preParkHomeGCODEs
+                : ''
+            );
+            const setPreParkExtrudeGCODEs = (
+                (pluginSettings.retract_before_park() != false)
+                ? preParkExtrudeGCODEs
+                : []
+            );
 
-			var preparkextrude0 = '';
-			var preparkextrude1 = '';
-			if (settings.retract_before_park() != false) {
-				preparkextrude0 = 'M83';
-				preparkextrude1 = 'G1 E-5 F50';
-			}
+            const zLiftRelative = pluginSettings.z_lift_relative();
+            const parkSpeed = pluginSettings.park_speed();
+            const parkCoordinates = {
+                x: pluginSettings.x_park(),
+                y: pluginSettings.y_park(),
+            };
+            const loadConfig = {
+                speed: pluginSettings.load_speed(),
+                length: pluginSettings.load_length(),
+            };
+            const unloadConfig = {
+                speed: pluginSettings.unload_speed(),
+                length: pluginSettings.unload_length(),
+            };
 
-			return [{
-				'customClass': '', 'layout': 'horizontal', 'name': 'Change Filament', 'children':[
-					{'commands': [
-						'M117 Parking nozzle',
-						preparkpause,
-						preparkextrude0,
-						preparkextrude1,
-						'G91',
-						'G0 Z' + settings.z_lift_relative() + ' F' + settings.park_speed(),
-						preparkhome,
-						'G90',
-						'G0 Y' + settings.y_park() + ' X' + settings.x_park() + ' F' + settings.park_speed(),
-						'M117 Nozzle parked'
-						],
-						'customClass': 'btn', 'additionalClasses': 'changefilament-park', 'name': ' Park'},
-					{'commands': [
-						'M117 Unloading filament',
-						'M83',
-						'G1 E-' + settings.unload_length() + ' F' + settings.unload_speed(),
-						'M18 E',
-						'M117 Replace filament, set new temp, click Load'
-						],
-						'customClass': 'btn', 'additionalClasses': 'changefilament-unload', 'name': ' Unload'},
-					{'commands': [
-						'M117 Loading filament',
-						'M83',
-						'G1 E' + settings.load_length() + ' F' + settings.load_speed(),
-						'M117 New Filament Loaded'
-						],
-						'customClass': 'btn', 'additionalClasses': 'changefilament-load', 'name': ' Load'},
-					{'commands': [
-						'M600'
-						],
-						'customClass': 'btn', 'additionalClasses': ' btn-danger changefilament-m600', 'name': ' M600'},
-					{'output': 'WARNING: Preheat first! Refresh page after changing settings.'},
-					{'output': 'M600 requires special support in Marlin and must be completed using the control box.'}
-				]
-			}];
+            return [{
+                'customClass': '',
+                'layout': 'horizontal',
+                'name': 'Change Filament',
+                'children': [
+                    {
+                        'commands': [
+                            'M117 Parking nozzle',
+                            ...setPreParkPauseGCODEs,
+                            ...setPreParkExtrudeGCODEs,
+                            'G91',
+                            'G0 Z' + zLiftRelative + ' F' + parkSpeed,
+                            ...setPreParkHomeGCODEs,
+                            'G90',
+                            'G0 Y' + parkCoordinates.y + ' X' + parkCoordinates.x + ' F' + parkSpeed,
+                            'M117 Nozzle parked',
+                        ],
+                        'customClass': 'btn',
+                        'additionalClasses': 'changefilament-park',
+                        'name': 'Park',
+                    },
+                    {
+                        'commands': [
+                            'M117 Unloading filament',
+                            'M83',
+                            'G1 E-' + unloadConfig.length + ' F' + unloadConfig.speed,
+                            'M18 E',
+                            'M117 Replace filament, set new temp, click Load',
+                        ],
+                        'customClass': 'btn',
+                        'additionalClasses': 'changefilament-unload',
+                        'name': 'Unload',
+                    },
+                    {
+                        'commands': [
+                            'M117 Loading filament',
+                            'M83',
+                            'G1 E' + loadConfig.length + ' F' + loadConfig.speed,
+                            'M117 New Filament Loaded',
+                        ],
+                        'customClass': 'btn',
+                        'additionalClasses': 'changefilament-load',
+                        'name': 'Load',
+                    },
+                    {
+                        'commands': [
+                            'M600'
+                        ],
+                        'customClass': 'btn',
+                        'additionalClasses': 'btn-danger changefilament-m600',
+                        'name': 'M600',
+                    },
+                    {
+                        'output': 'WARNING: Preheat first! Refresh page after changing settings.',
+                    },
+                    {
+                        'output': 'M600 requires special support in Marlin and must be completed using the control box.',
+                    },
+                ]
+            }];
+        };
+    }
 
-		};
-
-	}
-
-	OCTOPRINT_VIEWMODELS.push({
-		construct: Change_filamentViewModel,
-		dependencies: [ "settingsViewModel", "controlViewModel" ]
-	});
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Change_filamentViewModel,
+        dependencies: [ "settingsViewModel", "controlViewModel" ]
+    });
 });
